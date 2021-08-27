fd6050d861908021812f0da53c960236c27d9184
Himanshu <himanshu.m@menerva.tech>
2021-08-27 11:28:27 +0530

    new code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
+    <Router>
     <div className="App">
+    <Header />
      <Dashboard/>
+     <Route exact path="/addProject" Component={AddProject}/>
     </div>
+    </Router>
   );
 }
 
